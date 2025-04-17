bdcbbd0015eb74868d6540bed32eadaa62530ac5
Vinay Bosamiya <vinaybosamiya294@gmail.com>
2025-04-17 16:14:36 +0530

    Update doublylinkedlistmenudriven.c

--- doublylinkedlistmenudriven.c
+++ doublylinkedlistmenudriven.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-struct list {
+struct list
+{
     int info;
     struct list *next, *prev;
 };
 typedef struct list node;
-node *p = NULL;
+node *p;
+void create(int, node *);
+void display(node *);
+void addbeg(int, node *);
+void addafter(int, int, node *);
+void deleted(int, node *);
+#include <stdio.h>
+#include <stdlib.h>
 
-void create(int ele);
-void display();
-void addbeg(int ele);
-void addafter(int pos, int ele);
-void deleted(int ele);
-
-int main() {
+void main()
+{
     int choice, ele, pos;
+    p = NULL;
 
-    while (1) {
-        printf("\n\n--- MENU ---\n");
+    while (1)
+    {
+        printf("\n\n----- MENU -----\n");
         printf("1. Create Node\n");
         printf("2. Display List\n");
         printf("3. Add at Beginning\n");
@@ -28,144 +32,245 @@ int main() {
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
-        switch (choice) {
-            case 1:
-                printf("Enter element to create: ");
-                scanf("%d", &ele);
-                create(ele);
-                break;
+        switch (choice)
+        {
+        case 1:
+            printf("Enter element to insert: ");
+            scanf("%d", &ele);
+            create(ele, p);
+            break;
 
-            case 2:
-                printf("Doubly Linked List: ");
-                display();
-                break;
+        case 2:
+            printf("List elements:\n");
+            display(p);
+            break;
 
-            case 3:
-                printf("Enter element to add at beginning: ");
-                scanf("%d", &ele);
-                addbeg(ele);
-                break;
+        case 3:
+            printf("Enter element to add at beginning: ");
+            scanf("%d", &ele);
+            addbeg(ele, p);
+            break;
 
-            case 4:
-                printf("Enter position after which to insert: ");
-                scanf("%d", &pos);
-                printf("Enter element to insert: ");
-                scanf("%d", &ele);
-                addafter(pos, ele);
-                break;
+        case 4:
+            printf("Enter position after which to insert: ");
+            scanf("%d", &pos);
+            printf("Enter element to insert: ");
+            scanf("%d", &ele);
+            addafter(pos, ele, p);
+            break;
 
-            case 5:
-                printf("Enter element to delete: ");
-                scanf("%d", &ele);
-                deleted(ele);
-                break;
+        case 5:
+            printf("Enter element to delete: ");
+            scanf("%d", &ele);
+            deleted(ele, p);
+            break;
 
-            case 6:
-                printf("Exiting...");
-                exit(0);
+        case 6:
+            exit(0);
 
-            default:
-                printf("Invalid choice! Try again.");
+        default:
+            printf("Invalid choice. Try again.\n");
         }
     }
-
-    return 0;
 }
 
-void create(int ele) {
-    node *temp, *q;
-    if (p == NULL) {
+void create(int ele, node *q)
+{
+    node *temp;
+    if (q == NULL)
+    {
         p = (node *)malloc(sizeof(node));
         p->prev = NULL;
         p->info = ele;
         p->next = NULL;
-    } else {
-        q = p;
-        while (q->next != NULL) {
+    }
+    else
+    {
+        while (q->next != NULL)
+        {
             q = q->next;
         }
         temp = (node *)malloc(sizeof(node));
-        temp->info = ele;
         temp->next = NULL;
+        temp->info = ele;
         temp->prev = q;
         q->next = temp;
     }
 }
-
-void display() {
-    node *q = p;
-    if (q == NULL) {
-        printf("List is empty.");
-        return;
-    }
-    while (q != NULL) {
-        printf("%d ", q->info);
+void display(node *q)
+{
+    while (q != NULL)
+    {
+        printf("\t%d", q->info);
         q = q->next;
     }
 }
-
-void addbeg(int ele) {
-    node *temp = (node *)malloc(sizeof(node));
-    temp->info = ele;
-    temp->prev = NULL;
-    temp->next = p;
-    if (p != NULL)
-        p->prev = temp;
-    p = temp;
+void addbeg(int ele, node *q)
+{
+    p = (node *)malloc(sizeof(node));
+    p->prev = NULL;
+    p->info = ele;
+    p->next = q;
+    q->prev = p;
 }
-
-void addafter(int pos, int ele) {
-    node *q = p, *temp;
+void addafter(int c, int ele, node *q)
+{
+    node *temp;
     int i;
-    for (i = 1; i < pos; i++) {
-        if (q == NULL) {
-            printf("Position out of range.\n");
+    for (i = 1; i < c; i++)
+    {
+        q = q->next;
+        if (q == NULL)
+        {
+            printf("\nposition is out of range");
+            return;
+        }
+    }
+    temp = (node *)malloc(sizeof(node));
+    temp->prev = q;
+    temp->next = q->next;
+    temp->info = ele;
+    temp->next->prev = temp;
+    q->next = temp;
+    return;
+}
+void deleted(int ele, node *q)
+{
+    node *temp;
+    if (q->info == ele)
+    {
+        p = q->next;
+        q->next->prev = NULL;
+        free(q);
+        return;
+    }
+    while (q->next->next != NULL)
+    {
+        if (q->next->info == ele)
+        {
+            temp = q->next;
+            q->next = q->next->next;
+            q->next->prev = temp->prev;
+            free(temp);
             return;
         }
         q = q->next;
     }
-
-    if (q == NULL) {
-        printf("Position out of range.\n");
-        return;
-    }
-
-    temp = (node *)malloc(sizeof(node));
-    temp->info = ele;
-    temp->next = q->next;
-    temp->prev = q;
-    if (q->next != NULL)
-        q->next->prev = temp;
-    q->next = temp;
 }
 
-void deleted(int ele) {
-    node *q = p, *temp;
 
-    if (q == NULL) {
-        printf("List is empty.\n");
-        return;
-    }
 
-    if (q->info == ele) {
-        p = q->next;
-        if (p != NULL)
-            p->prev = NULL;
-        free(q);
-        return;
-    }
 
-    while (q->next != NULL && q->next->info != ele)
-        q = q->next;
 
-    if (q->next == NULL) {
-        printf("Element not found.\n");
-        return;
-    }
 
-    temp = q->next;
-    q->next = temp->next;
-    if (temp->next != NULL)
-        temp->next->prev = q;
-    free(temp);
-}
+
+// output
+
+/*
+
+----- MENU -----
+1. Create Node       
+2. Display List      
+3. Add at Beginning  
+4. Add After Position
+5. Delete Element    
+6. Exit
+Enter your choice: 1 
+Enter element to insert: 1
+
+
+----- MENU -----
+1. Create Node
+2. Display List
+3. Add at Beginning
+4. Add After Position
+5. Delete Element
+6. Exit
+Enter your choice: 1
+Enter element to insert: 2
+
+
+----- MENU -----
+1. Create Node
+2. Display List
+3. Add at Beginning
+4. Add After Position
+5. Delete Element
+6. Exit
+Enter your choice: 1
+Enter element to insert: 3
+
+
+----- MENU -----
+1. Create Node
+2. Display List
+3. Add at Beginning
+4. Add After Position
+5. Delete Element
+6. Exit
+Enter your choice: 2
+List elements:
+        1       2       3
+
+----- MENU -----
+1. Create Node
+2. Display List
+3. Add at Beginning
+4. Add After Position
+5. Delete Element
+6. Exit
+Enter your choice: 3
+Enter element to add at beginning: 22
+
+
+----- MENU -----
+1. Create Node
+2. Display List
+3. Add at Beginning
+4. Add After Position
+5. Delete Element
+6. Exit
+Enter your choice: 2
+List elements:
+        22      1       2       3
+
+----- MENU -----
+1. Create Node
+2. Display List
+3. Add at Beginning
+4. Add After Position
+5. Delete Element
+6. Exit
+Enter your choice: 4
+Enter position after which to insert: 3
+Enter element to insert: 55
+
+
+----- MENU -----
+1. Create Node
+2. Display List
+3. Add at Beginning
+4. Add After Position
+5. Delete Element
+6. Exit
+Enter your choice: 2
+List elements:
+        22      1       2       55      3
+
+----- MENU -----
+1. Create Node
+2. Display List
+3. Add at Beginning
+4. Add After Position
+5. Delete Element
+6. Exit
+Enter your choice: 6
+
+
+
+
+
+
+
+
+*/
